e78582cd681e9dcfb19b1f1cc705ec64b25ad003
Shreyas Jadhav <shreyasjadhav444@gmail.com>
2024-11-28 10:48:44 +0530

    params

--- src/components/SpeakPage/CreateRecordingForm/LoopingRecording/useRealtimePlayback.ts
+++ src/components/SpeakPage/CreateRecordingForm/LoopingRecording/useRealtimePlayback.ts
@@ -1,5 +1,29 @@
 import { useEffect } from 'react';
 
+/*
+
+Vol: 1
+Delay: 0
+Reverb: 0.5
+Threshold: -20
+Knee: 20
+Ratio: 12
+Attack: 10ms
+Release: 250ms
+
+*/
+
+const PARAMS = {
+	vol: 1,
+	delay: 0,
+	reverb: 0.5,
+	threshold: -20,
+	knee: 20,
+	ratio: 12,
+	attack: 0.01,
+	release: 0.25,
+};
+
 export const useRealtimePlayback = ({ audioContext, recordingStream }: { audioContext: AudioContext; recordingStream?: MediaStream }) => {
 	// you need to check on the recordingStream every time it changes.
 	// also when unmounting,
